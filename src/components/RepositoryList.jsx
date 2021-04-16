fec07b2492c26fb67d320dc9b802b9e3ab0b453a
Leonardo Costa <66449040+leonardocosta95@users.noreply.github.com>
2021-04-16 18:37:48 -0300

    listando repositorios

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,30 @@
+import { useState, useEffect } from 'react';
+import { RepositoryItem } from './RepositoryItem';
+import '../styles/repositories.scss';
+
+// https://api.github.com/orgs/rocketseat/repos
+
+
+export function RepositoryList() {
+
+    const [repositories, setRepositories] = useState([])
+
+    useEffect(() => {
+        fetch('https://api.github.com/orgs/rocketseat/repos')
+        .then(response => response.json())
+        .then(data => setRepositories(data))
+    }, []); // não deixar a segunda posição vazia, para não entrar em looping
+
+    return (
+        <section className="repository-list">
+            <h1>Lista de repositórios</h1>
+
+            <ul>
+                {repositories.map(repository => {
+                    return <RepositoryItem key={repository.name} repository={repository}/>
+                })}
+                
+            </ul>
+        </section>
+    )
+}
